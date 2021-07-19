1a04132b0c58a3f133e6bb33234af235a8a04941
starquf <starquf321@gmail.com>
2021-07-19 19:19:31 +0900

    게임매니져, 풀매니져, 이벤트매니져(현재 테스트중)등등 추가

---
+++ Game_Jam/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region SingleTon
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+
+                if (instance == null)
+                {
+                    Debug.LogError("게임메니져가 존재하지 않습니다!");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)                       // 만약 instance가 비어있다면
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if(instance != this)                   // 비어있진 않은데 instance가 자신이 아니라면
+        {
+            Destroy(this.gameObject);
+        }
+    }
+#endregion
+
+
+}
