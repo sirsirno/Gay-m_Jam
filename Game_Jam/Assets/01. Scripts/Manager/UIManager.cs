ed684130d023562e3c10f3b7a43e8c671394b48f
Double_U_ <junhyup511@naver.com>
2021-07-27 00:16:49 +0900

    체인 파티클 추가

--- Game_Jam/Assets/01. Scripts/Manager/UIManager.cs
+++ Game_Jam/Assets/01. Scripts/Manager/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
@@ -17,6 +18,10 @@ public class UIManager : MonoBehaviour
 
     [SerializeField] RectTransform hpBar;
 
+    [SerializeField] Image chainUI;
+    [SerializeField] ParticleSystem chainParticle;
+    Coroutine chainEndCoroutine;
+
     private void Start()
     {
         SetWaveNumber(15);
@@ -26,6 +31,11 @@ public class UIManager : MonoBehaviour
     private void Update()
     {
         SetHpFill();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RefreshChainUI();
+        }
     }
 
     public void SetHpFill()
@@ -73,5 +83,33 @@ public class UIManager : MonoBehaviour
             left1Num.gameObject.SetActive(true);
             left1Num.sprite = DefaultNumSprites[leftNum % 10];
         }
+    }
+
+    public void RefreshChainUI()
+    {
+        chainUI.transform.DOKill();
+        chainParticle.transform.DOKill();
+        chainUI.DOKill();
+
+        chainUI.transform.localScale = new Vector2(0, 1);
+        chainUI.color = new Color(1, 1, 1, 0);
+
+        chainUI.transform.DOScaleX(1, 0.25f);
+        chainParticle.transform.DOScale(1, 0.25f);
+        chainUI.DOFade(1, 0.25f);
+
+        if (chainEndCoroutine != null)
+        {
+            StopCoroutine(chainEndCoroutine);
+        }
+        chainEndCoroutine = StartCoroutine(ChainEnd());
+    }
+
+    IEnumerator ChainEnd()
+    {
+        yield return new WaitForSeconds(4f);
+        chainUI.transform.DOScaleY(0, 0.5f);
+        chainParticle.transform.DOScale(0, 0.5f);
+        chainUI.DOFade(0, 0.5f);
     }
 }
