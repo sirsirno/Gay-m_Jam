2490b8815a7013754667b0e60031d998034bd7fa
JUGU <qheorjsdnr@naver.com>
2021-07-27 15:35:55 +0900

    타이틀 UI

--- Game_Jam/Assets/01. Scripts/Intro/TitleScene.cs
+++ Game_Jam/Assets/01. Scripts/Intro/TitleScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class TitleScene : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class TitleScene : MonoBehaviour
 
     [SerializeField] OptionUIManager optionUIManager;
     [SerializeField] private Button startButton;
+    [SerializeField] private Text startText;
+    [SerializeField] private GameObject stageBackground;
+    [SerializeField] private GameObject stages;
+    [SerializeField] private GameObject Txts;
+    [SerializeField] private List<GameObject> stageTxts = new List<GameObject>();
     private bool isTitle;
 
     private void Awake()
@@ -36,13 +42,22 @@ public class TitleScene : MonoBehaviour
         isTitle = true;
         startButton.gameObject.SetActive(true);
         startButton.onClick.AddListener(() => { ClickToStart(); });
+        startText.DOColor(new Color(0f, 0f, 0f, 0f), 2f).SetLoops(-1, LoopType.Yoyo);
 
         audioSource.Play();
     }
 
     void ClickToStart() 
     {
-        SceneController.LoadScene("InGame");
         isTitle = false;
+        startText.gameObject.SetActive(false);
+        stages.SetActive(true);
+        stageBackground.SetActive(true);
+        stages.transform.DOLocalMoveX(600f, 1f);
+        Txts.SetActive(true);
+        stageTxts[0].transform.DOLocalMoveX(-50f, 1f);
+        stageTxts[1].transform.DOLocalMoveX(-50f, 2f);
+        stageTxts[2].transform.DOLocalMoveX(-50f, 3f);
+        stageTxts[3].transform.DOLocalMoveX(-50f, 4f);
     }
 }
